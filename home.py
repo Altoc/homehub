10b8267747f99bd8c72123ac1ca8f6a926d946df
Ian Whitesel <iwhitese@kent.edu>
2019-05-30 11:49:01 -0400

    adding 05/30/2019

--- home.py
+++ home.py
@@ -1,20 +1,24 @@
 import tkinter
 from tkinter import *
+from tkinter import messagebox
 import homehubdb
 print("home.py called")
 
 db = homehubdb.Db_manager()
 
 root = tkinter.Tk()
+root.geometry("512x512")
 
-myStr = StringVar()
-label = Label(root, textvariable = myStr)
+def userSelect():
+    userInfo = messagebox.showinfo("user", "info")
 
-db.cursor.execute("SELECT * FROM users")
+db.cursor.execute("SELECT name FROM users")
 
 for val in db.cursor.fetchall():
-    myStr.set(val)
-
-label.pack()
+    str = StringVar()
+    str.set(val)
+    userButton = Button(root, textvariable = str, command = userSelect)
+    userButton.place(x = 50, y = 50)
+    userButton.pack()
 
 root.mainloop()
