fbbff39c366875f1db25a422a4cd195dfdc0be6b
Ian Whitesel <iwhitese@kent.edu>
2019-06-24 16:38:29 -0400

    adding notes functionality initial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#TODO: Set subject of grocery emails | END CONNECTIONS TO DB AND ONLY CONNECT DB WHEN USING IT
+#TODO: Set subject of emails | delete notes from one's note screen
 import tkinter
 from tkinter import *
 from tkinter import messagebox
@@ -69,12 +69,10 @@ class FR_login:
                 passW = val[0]
             db.disconnect()
             if passW == self.attemptedPassword:
-                print("password match!")
                 for element in root.winfo_children():
                     element.destroy()
                 mainApp = FR_home(self.usernameSelected)
             else:
-                print("passwords did not match")
                 self.shownAttemptedPassw_var.set("Wrong... Try Again")
             self.attemptedPassword = str()
 
@@ -83,20 +81,66 @@ class FR_home:
         global root
         self.homeRoot = Frame(root)
         self.homeRoot.grid()
+        self.notesFrame = Frame(root)
+        self.notesFrame.grid()
         root.title("Home")
         self.username = username
         greeting = Label(self.homeRoot, text = "Welcome to HomeHub6270, {}".format(self.username))
         greeting.grid()
         groceryButton = Button(self.homeRoot, text="Grocery List", command=lambda : self.grocery())
         groceryButton.grid()
+        notesButton = Button(self.homeRoot, text="Leave a Note", command=lambda : self.notes())
+        notesButton.grid()
         self.backToLogin = Button(self.homeRoot, text="LOGOUT", command=self.logout)
         self.backToLogin.grid()
+        db = homehubdb.Db_manager()
+        db.cursor.execute("SELECT id FROM users WHERE name=%s", (self.username))
+        dbReturn = db.cursor.fetchall()
+        for val in dbReturn:
+            self.userID = val[0]
+        db.disconnect()
+        self.populateNotes()
 
     def logout(self):
         global root
         for element in root.winfo_children():
             element.destroy()
         mainApp = FR_login()
+
+    def notes(self):
+        self.notesPopup = tkinter.Tk()
+        self.notesPopup.wm_title("Leave A Note")
+        notesPrompt = Label(self.notesPopup, text="What would you like to say?")
+        notesPrompt.grid()
+        self.notesEntry = Entry(self.notesPopup)
+        self.notesEntry.grid()
+        notesEntry_button = Button(self.notesPopup, text="Say It!", command=self.addNote)
+        notesEntry_button.grid()
+
+    def addNote(self):
+        db = homehubdb.Db_manager()
+        db.cursor.execute("INSERT INTO notes(message,date,name) VALUES(%s, NOW(), %s)", (self.notesEntry.get(), self.username))
+        db.hubdb.commit()
+        self.notesEntry.delete(0, 'end')
+        db.disconnect()
+        self.notesPopup.destroy()
+        self.populateNotes()
+
+    def populateNotes(self):
+        for element in self.notesFrame.winfo_children():
+            element.destroy()
+        db = homehubdb.Db_manager()
+        db.cursor.execute("SELECT name, message FROM notes WHERE date BETWEEN NOW() - INTERVAL 7 DAY AND NOW()")
+        for val in db.cursor.fetchall():
+            notedb = StringVar()
+            notedb.set(val[1])
+            messageAuthor = StringVar()
+            messageAuthor.set(val[0])
+            noteLabel = Label(self.notesFrame, textvariable=notedb)
+            noteLabel.grid(column=0)
+            authorLabel = Label(self.notesFrame, textvariable=messageAuthor)
+            authorLabel.grid(column=1)
+        db.disconnect()
 
     def grocery(self):
         global mainApp
@@ -115,16 +159,15 @@ class FR_grocery:
         self.grocRoot.grid()
         self.listFrame.grid()
         self.entryFrame.grid()
-
-        self.username = username
-        greeting = Label(self.grocRoot, text = "{}'s Grocery List".format(self.username))
-        greeting.grid()
         db = homehubdb.Db_manager()
         db.cursor.execute("SELECT id FROM users WHERE name=%s", (self.username))
         dbReturn = db.cursor.fetchall()
         for val in dbReturn:
             self.userID = val[0]
         db.disconnect()
+        self.username = username
+        greeting = Label(self.grocRoot, text = "{}'s Grocery List".format(self.username))
+        greeting.grid()
         self.populateGroceryList()
         self.populateUI()
 
@@ -153,7 +196,6 @@ class FR_grocery:
         self.back_button.grid()
 
     def addGroceryItem(self):
-        print("Adding {} to list...".format(self.entry_1.get()))
         db = homehubdb.Db_manager()
         db.cursor.execute("INSERT INTO GroceryList(id,item) VALUES(%s, %s)", (self.userID, self.entry_1.get()))
         db.hubdb.commit()
