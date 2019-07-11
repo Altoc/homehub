e2c0442ba83bb9ba9ab93b8d064e86e3f759d6d8
Ian Whitesel <iwhitese@kent.edu>
2019-07-11 12:47:01 -0400

    Adding setting page w/ account info changing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#TODO: Set subject of emails | delete notes from one's note screen
+#TODO: Set subject of emails | limit input boxes for password settings
 import tkinter
 from tkinter import *
 from tkinter import messagebox
@@ -85,14 +85,7 @@ class FR_home:
         self.notesFrame.grid()
         root.title("Home")
         self.username = username
-        greeting = Label(self.homeRoot, text = "Welcome to HomeHub6270, {}".format(self.username))
-        greeting.grid()
-        groceryButton = Button(self.homeRoot, text="Grocery List", command=lambda : self.grocery())
-        groceryButton.grid()
-        notesButton = Button(self.homeRoot, text="Leave a Note", command=lambda : self.notes())
-        notesButton.grid()
-        self.backToLogin = Button(self.homeRoot, text="LOGOUT", command=self.logout)
-        self.backToLogin.grid()
+        self.populateUI()
         db = homehubdb.Db_manager()
         db.cursor.execute("SELECT id FROM users WHERE name=%s", (self.username))
         dbReturn = db.cursor.fetchall()
@@ -100,6 +93,18 @@ class FR_home:
             self.userID = val[0]
         db.disconnect()
         self.populateNotes()
+
+    def populateUI(self):
+        greeting = Label(self.homeRoot, text = "Welcome to HomeHub6270, {}".format(self.username))
+        greeting.grid()
+        groceryButton = Button(self.homeRoot, text="Grocery List", command=lambda : self.grocery())
+        groceryButton.grid()
+        notesButton = Button(self.homeRoot, text="Leave a Note", command=lambda : self.notes())
+        notesButton.grid()
+        self.settingsButton = Button(self.homeRoot, text="Account Settings", command=self.settings)
+        self.settingsButton.grid()
+        self.backToLogin = Button(self.homeRoot, text="LOGOUT", command=self.logout)
+        self.backToLogin.grid()
 
     def logout(self):
         global root
@@ -148,6 +153,13 @@ class FR_home:
         for element in root.winfo_children():
             element.destroy()
         mainApp = FR_grocery(self.username)
+
+    def settings(self):
+        global mainApp
+        global root
+        for element in root.winfo_children():
+            element.destroy()
+        mainApp = FR_settings(self.username)
 
 class FR_grocery:
     def __init__(self, username):
@@ -223,6 +235,94 @@ class FR_grocery:
         em.setMsg(emailList)
         em.sendMsg(emailRecip)
         db.disconnect()
+
+    def backToHome(self):
+        global mainApp
+        global root
+        for element in root.winfo_children():
+            element.destroy()
+        mainApp = FR_home(self.username)
+
+class FR_settings():
+    def __init__(self, username):
+        global root
+        root.title("Settings")
+        self.username = username
+        self.settingsRoot = Frame(root)
+        self.settingsRoot.grid()
+        self.accountInfoFrame = Frame(root)
+        self.accountInfoFrame.grid()
+        self.populateAccountInfo()
+        #self.populateUI()
+
+    def populateAccountInfo(self):
+        for element in self.accountInfoFrame.winfo_children():
+            element.destroy()
+        db = homehubdb.Db_manager()
+        db.cursor.execute("SELECT * FROM users WHERE name=%s", (self.username))
+        for val in db.cursor.fetchall():
+            session_id = val[0]		#INT
+            session_name = val[1]	#str
+            session_password = val[2]	#str
+            session_email = val[3]	#str
+            session_phone = val[4]	#str
+        db.disconnect()
+        packedSettingsStr = "Your Account Number: " + str(session_id) + '\n' + \
+                            "Your Preferred Name: " + session_name + '\n' + \
+                            "Your Primary Email: " + session_email + '\n' + \
+                            "Your Primary Cellphone: " + session_phone
+        packedSettingsStrVar = StringVar()
+        packedSettingsStrVar.set(packedSettingsStr)
+        accountSettingsInfoLabel = Label(self.accountInfoFrame, textvariable=packedSettingsStrVar)
+        accountSettingsInfoLabel.grid()
+        self.populateUI()
+
+    def populateUI(self):
+        updateSettingsButton = Button(self.accountInfoFrame, text="Update My Info", command=self.createInfoPrompt)
+        updateSettingsButton.grid()
+        self.back_button = Button(self.accountInfoFrame, text="Back To Home", command = self.backToHome)
+        self.back_button.grid()
+
+    def createInfoPrompt(self):
+        self.infoPopup = tkinter.Tk()
+        self.infoPopup.wm_title("Update My Information")
+        passInfoLabel = Label(self.infoPopup, text="PASSWORDS MUST BE 4 DIGIT NUMBER COMBOS \n IF YOU DID SOMETHING ELSE TALK TO IAN \n IM WORKING ON CHANGING THAT")
+        passInfoLabel.grid(row=0, column=0)
+        passPrompt = Label(self.infoPopup, text="Update Password")
+        passPrompt.grid(row=1, column=0)
+        self.passEntry = Entry(self.infoPopup)
+        self.passEntry.grid(row=1, column=1)
+        emailPrompt = Label(self.infoPopup, text="Update Email")
+        emailPrompt.grid(row=2, column=0)
+        self.emailEntry = Entry(self.infoPopup)
+        self.emailEntry.grid(row=2, column=1)
+        phonePrompt = Label(self.infoPopup, text="Update Phone")
+        phonePrompt.grid(row=3, column=0)
+        self.phoneEntry = Entry(self.infoPopup)
+        self.phoneEntry.grid(row=3, column=1)
+        infoEntry_button = Button(self.infoPopup, text="Update", command=lambda : self.updateInfo())
+        infoEntry_button.grid()
+
+    def updateInfo(self):
+        db = homehubdb.Db_manager()
+
+        if len(self.passEntry.get()) != 0:
+            executeStmt = str(self.passEntry.get())
+            db.cursor.execute("UPDATE users SET password=%s WHERE name=%s", (executeStmt, self.username))
+        if len(self.emailEntry.get()) != 0:
+            executeStmt = str(self.emailEntry.get())
+            db.cursor.execute("UPDATE users SET email=%s WHERE name=%s", (executeStmt, self.username))
+        if len(self.phoneEntry.get()) != 0:
+            executeStmt = str(self.phoneEntry.get())
+            db.cursor.execute("UPDATE users SET phone=%s WHERE name=%s", (executeStmt, self.username))
+
+        db.hubdb.commit()
+        db.disconnect()
+        self.passEntry.delete(0, 'end')
+        self.emailEntry.delete(0, 'end')
+        self.phoneEntry.delete(0, 'end')
+        self.infoPopup.destroy()
+        self.populateAccountInfo()
 
     def backToHome(self):
         global mainApp
