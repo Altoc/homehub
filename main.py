9906d5f28228f750e3d369d2862a191d0a5a851c
Ian Whitesel <iwhitese@kent.edu>
2019-07-17 17:42:20 -0400

    Adding style changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #TODO: Set subject of emails
 #TODO: limit input boxes for password settings
 #TODO: Make Pizza Purchase Tracker (have it ask each person who bought pizza last to confirm)
+#TODO: Have notes frame scroll, takes a bit of tinkering with tkinter to do this
 import tkinter
 from tkinter import *
 from tkinter import messagebox
@@ -23,22 +24,28 @@ class FR_login:
         self.shownAttemptedPassw_var = StringVar()
         self.usernameSelected = str()
         self.usernameFrame.pack(side = TOP)
-        self.passwFrame.pack()
         self.keypadFrame.pack(side = BOTTOM)
-        greeting = Label(self.usernameFrame, text="Please Login")
-        greeting.grid(row=0, column=0)
+        self.passwFrame.pack(side = BOTTOM)
+        greeting1 = Label(self.usernameFrame, text="HOME HUB 6270")
+        greeting1.config(font=("Times", 32, "bold"))
+        greeting1.grid()
+        greeting2 = Label(self.usernameFrame, text="Please Login...")
+        greeting2.config(font=("Times", 16, "italic"))
+        greeting2.grid()
         db = homehubdb.Db_manager()
         db.cursor.execute("SELECT name, id FROM users")
-        loginUserCounter = 1
+        loginUserCounter = 2
         for val in db.cursor.fetchall():
             dbUsername = StringVar()
             dbUsername.set(val[0])
             userButton = Button(self.usernameFrame, textvariable = dbUsername, command=lambda dbUsername=dbUsername: self.userSelect(dbUsername))
+            userButton.config(width=16)
             userButton.grid(row=loginUserCounter, column=0)
             # V cursor.execute returns an int of affected rows V
             unreadCheck = db.cursor.execute("SELECT noteID, unread FROM userNotes WHERE userID=%s AND unread=1", (val[1]))
             if(unreadCheck > 0):
                 unreadMessages = Label(self.usernameFrame, text="*NEW NOTES*")
+                unreadMessages.config(width=16,font=("Helvetica", 16, "bold"))
                 unreadMessages.grid(row=loginUserCounter, column=1)
             loginUserCounter = loginUserCounter + 1
         db.disconnect()
@@ -54,12 +61,12 @@ class FR_login:
             for j in range(3):
                 numCounterStr = str(numCounter)
                 passwordNumpad = Button(self.keypadFrame, text=numCounterStr, command=lambda numCounterStr=numCounterStr: self.userAuthentication(numCounterStr)) #making this a lambda allows passing of params
-                passwordNumpad.config(height=4, width=4)
+                passwordNumpad.config(height=2, width=2)
                 numCounter = numCounter + 1
                 passwordNumpad.grid(row=i, column=j)
                 root.update()
         zeroButton = Button(self.keypadFrame, text="0", command=lambda : self.userAuthentication("0"))
-        zeroButton.config(height=4, width=4)
+        zeroButton.config(height=2, width=2)
         zeroButton.grid(row=4, column=1)
 
     def userAuthentication(self, numToAppend):
@@ -89,9 +96,11 @@ class FR_home:
     def __init__(self, username):
         global root
         self.homeRoot = Frame(root)
-        self.homeRoot.grid()
+        self.homeRoot.pack(side=TOP)
+        self.menuFrame = Frame(root)
+        self.menuFrame.pack(side=TOP)
         self.notesFrame = Frame(root)
-        self.notesFrame.grid()
+        self.notesFrame.pack(side=BOTTOM)
         root.title("Home")
         self.username = username
         self.populateUI()
@@ -105,15 +114,28 @@ class FR_home:
 
     def populateUI(self):
         greeting = Label(self.homeRoot, text = "Welcome to HomeHub6270, {}".format(self.username))
-        greeting.grid()
-        groceryButton = Button(self.homeRoot, text="Grocery List", command=lambda : self.grocery())
-        groceryButton.grid()
-        notesButton = Button(self.homeRoot, text="Leave a Note", command=lambda : self.notes())
-        notesButton.grid()
-        self.settingsButton = Button(self.homeRoot, text="Account Settings", command=self.settings)
-        self.settingsButton.grid()
-        self.backToLogin = Button(self.homeRoot, text="LOGOUT", command=self.logout)
-        self.backToLogin.grid()
+        greeting.config(font=("Times", 16, "italic"))
+        greeting.grid(row=0,column=2)
+
+        groceryButton = Button(self.menuFrame, text="Grocery List", command=lambda : self.grocery())
+        groceryButton.config(height=2,width=12)
+        groceryButton.grid(row=1,column=0)
+
+        notesButton = Button(self.menuFrame, text="Leave a Note", command=lambda : self.notes())
+        notesButton.config(height=2,width=12)
+        notesButton.grid(row=1,column=1)
+
+        self.settingsButton = Button(self.menuFrame, text="Account Settings", command=self.settings)
+        self.settingsButton.config(height=2,width=12)
+        self.settingsButton.grid(row=1,column=2)
+
+        self.billsButton = Button(self.menuFrame, text="Bill Manager")
+        self.billsButton.config(height=2,width=12)
+        self.billsButton.grid(row=1,column=3)
+
+        self.backToLogin = Button(self.menuFrame, text="LOGOUT", command=self.logout)
+        self.backToLogin.config(height=2,width=12)
+        self.backToLogin.grid(row=2,column=0)
 
     def logout(self):
         global root
@@ -154,6 +176,9 @@ class FR_home:
             db.cursor.execute("DELETE FROM userNotes WHERE noteID=%s", (val[0]))
             db.cursor.execute("DELETE FROM notes WHERE id=%s", (val[0]))
         db.hubdb.commit()
+        noteTitleLabel = Label(self.notesFrame, text="A note for you...")
+        noteTitleLabel.config(font=("Times",16,"italic"))
+        noteTitleLabel.grid()
         db.cursor.execute("SELECT name, message, dateCreated, id FROM notes WHERE dateCreated BETWEEN NOW() - INTERVAL 7 DAY AND NOW()")
         for val in db.cursor.fetchall():
             notedb = StringVar()
@@ -189,13 +214,13 @@ class FR_home:
 class FR_grocery:
     def __init__(self, username):
         global root
-        self.grocRoot = Frame(root)
         root.title("Groceries")
+        self.grocRoot = Frame(root)
         self.listFrame = Frame(root)
         self.entryFrame = Frame(root)
-        self.grocRoot.grid()
-        self.listFrame.grid()
-        self.entryFrame.grid()
+        self.grocRoot.pack(side=TOP)
+        self.listFrame.pack()
+        self.entryFrame.pack()
         self.username = username
         db = homehubdb.Db_manager()
         db.cursor.execute("SELECT id FROM users WHERE name=%s", (self.username))
@@ -204,6 +229,7 @@ class FR_grocery:
             self.userID = val[0]
         db.disconnect()
         greeting = Label(self.grocRoot, text = "{}'s Grocery List".format(self.username))
+        greeting.config(font=("Times",16,"bold"))
         greeting.grid()
         self.populateGroceryList()
         self.populateUI()
@@ -228,13 +254,21 @@ class FR_grocery:
     def populateUI(self):
         self.entry_1 = Entry(self.entryFrame)
         self.entry_1.grid(row=0, column=0)
+
         self.entry_button_1 = Button(self.entryFrame, text="Add Item", command = self.addGroceryItem)
+        self.entry_button_1.config(height=1,width=8)
         self.entry_button_1.grid(row=0,column=1)
+
         self.delete_list_button = Button(self.entryFrame, text="Delete List", command = self.deleteGroceryList)
+        self.delete_list_button.config(height=1,width=8)
         self.delete_list_button.grid(row=1,column=0)
+
         self.email_list_button = Button(self.entryFrame, text="Email List", command = self.emailList)
+        self.email_list_button.config(height=1,width=8)
         self.email_list_button.grid(row=2,column=0)
-        self.back_button = Button(self.entryFrame, text="Back To Home", command = self.backToHome)
+
+        self.back_button = Button(self.entryFrame, text="Home", command = self.backToHome)
+        self.back_button.config(height=1,width=8)
         self.back_button.grid(row=3,column=0)
 
     def addGroceryItem(self):
@@ -342,7 +376,6 @@ class FR_settings():
 
     def updateInfo(self):
         db = homehubdb.Db_manager()
-
         if len(self.passEntry.get()) != 0:
             executeStmt = int(str(self.passEntry.get()))
             db.cursor.execute("UPDATE users SET password=%s WHERE name=%s", (executeStmt, self.username))
@@ -352,7 +385,6 @@ class FR_settings():
         if len(self.phoneEntry.get()) != 0:
             executeStmt = str(self.phoneEntry.get())
             db.cursor.execute("UPDATE users SET phone=%s WHERE name=%s", (executeStmt, self.username))
-
         db.hubdb.commit()
         db.disconnect()
         self.passEntry.delete(0, 'end')
